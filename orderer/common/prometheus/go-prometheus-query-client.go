677d130f96a9154f213a5811ab14142aad1e4032
Xhens Basha <xhens.basha@gmail.com>
2020-05-10 18:36:24 +0200

    [ASHF-48] create some core functions for querying prometheus

--- orderer/common/prometheus/go-prometheus-query-client.go
+++ orderer/common/prometheus/go-prometheus-query-client.go
@@ -1,32 +1,76 @@
 package prometheus
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
+	"strconv"
+	"time"
 )
 
-var url = "http://localhost:9090/"
-var apiVersion = "api/v1/"
-var query = "query?query="
+var baseUrl = "http://localhost:9090"
+var SINGLE = "/api/v1/query"
+var RANGE = "/api/v1/query_range"
 
-func prometheusGet() {
+func getPrometheusData() string {
 
-	var resp, err = http.Get(url + apiVersion + query + "ledger_blockchain_height")
+	var resp, err = http.Get(baseUrl + SINGLE + "ledger_blockchain_height")
 	if err != nil {
 		log.Fatalln(err)
 	}
 
+	// get data from the response body
 	defer resp.Body.Close()
 
-	body, error := ioutil.ReadAll(resp.Body)
-	if error != nil {
-		log.Fatalln(error)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
 	}
-
-	log.Println(string(body))
+	return string(body)
 }
 
-func Main() {
-	prometheusGet()
+func retrieveTargetParameters(queryType string, query string) *url.URL {
+	base, _ := url.Parse(baseUrl) // domain = localhost:9090
+	relativeUrl, _ := url.Parse(queryType) // api/v1/query or api/v1/query_range
+	queryStringParams := relativeUrl.Query()
+	params, _ := url.ParseQuery(query)
+	for key, value := range params {
+		queryStringParams.Set(key, value[0])
+	}
+	relativeUrl.RawQuery = queryStringParams.Encode()
+	fullUrl := base.ResolveReference(relativeUrl)
+	fmt.Println(fullUrl)
+	return fullUrl
+}
+
+func query(queryString string, timePoint int64) *url.URL {
+	queryStringParams := url.Values{}
+	queryStringParams.Set("query", queryString)
+	queryStringParams.Set("time", strconv.FormatInt(timePoint, 10))
+	encodedQueryStringParams := queryStringParams.Encode()
+	return retrieveTargetParameters(SINGLE, encodedQueryStringParams)
+}
+
+func rangeQuery(queryString string, startTime int64, endTime int64, step int) *url.URL {
+	// url.Values will map values by sorting keys alphabetically
+	qsParams := url.Values{}
+	qsParams.Set("query", queryString)
+	qsParams.Set("start", strconv.FormatInt(startTime, 10))
+	qsParams.Set("end", strconv.FormatInt(endTime, 10))
+	qsParams.Set("step", strconv.Itoa(step))
+	encodedQueryStringParams := qsParams.Encode()
+	return retrieveTargetParameters(RANGE, encodedQueryStringParams)
+}
+
+
+func readPrometheusData() {
+	// data := getPrometheusData()
+	// fmt.Println(data)
+	timeNow := time.Now()
+	endTimeUnix := timeNow.Unix()
+	startTimeUnix := timeNow.Add(-time.Minute * 30).Unix() // 30 minutes earlier
+	query("ledger_blockchain_height", startTimeUnix)
+	rangeQuery("ledger_blockchain_height", startTimeUnix, endTimeUnix,7)
 }
